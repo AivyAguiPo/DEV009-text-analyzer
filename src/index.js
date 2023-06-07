c6cc6a9d96cd2c62fb7f6f0760049e0e31ac0314
Aivy Aguirre <aivy3094@gmail.com>
2023-06-07 01:10:34 -0500

    Subiendo cambios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const areatexto = document.getElementsByName('user-input')[0];
+const resetbutton = document.getElementById('reset-button');
+//borrar todo
+resetbutton.addEventListener('click', function () { areatexto.value = "" });
+
+areatexto.addEventListener('keyup', function () {
+    const li1 = document.querySelector('.box');
+
+
+
+});
+
+
+
+
+
