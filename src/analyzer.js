8f3464be35541b3696e0f47db5f067df65c769d9
Aivy Aguirre <aivy3094@gmail.com>
2023-06-13 01:03:58 -0500

    Subiendo

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,54 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    alert("text")
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
   getCharacterCount: (text) => {
+    return text.length; //*length devuelve el largo del string incluido el espacio osea el numero de elementos
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
+    const elementx =text.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+    const CharacterCountExcludingSpaces = elementx.length;
+    return CharacterCountExcludingSpaces;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (text) => { 
+    const words =text.split(" ");//text es el texto ,split divide el texto
+    const wordCount =words.length;//No es lo mismo que esta en Index.js 
+    return wordCount;  
+    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
+    const elements =text.split(" "); // separa los elementos por espacios like[al , be , 1 , 2, 13]
+    const elementsCount =elements.length;//cuenta los elementos
+    const numbers =[]; //declaro un arreglo de numeros vacios -numbers mi array
+    for (let i= 0; i< elementsCount; i++){
+      const element= Number(elements[i]);
+      if (!isNaN(element)&&element){numbers.push(element)}//valido si es numero 
+    }
+    const numberCount=numbers.length;
+    return numberCount
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+    const elements =text.split(" "); // separa los elementos por espacios like[al , be , 1 , 2, 13]
+    const elementsCount =elements.length;//cuenta los elementos
+    let total = 0; 
+    for (let i= 0; i< elementsCount; i++){
+      const element= Number(elements[i]);
+      if (!isNaN(element)){total+=element}
+    }
+    return total
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  },
+  getAverageWordLength: (text) => {    
+    const wordL = text.split(" ");//separa los elementos por espacios like[al , be , 1 , 2, 13]
+    let lengthTotal = 0;
+    for (let i=0; i < wordL.length; i++){
+      lengthTotal += wordL[i].length;
+    }
+    const averageWord = lengthTotal / wordL.length; 
+    const averageWordL = averageWord.toFixed(2);
+    const averageWordFinal = Number(averageWordL);
+    return averageWordFinal;
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ import analyzer from './analyzer.js';
 
 const areatexto = document.getElementsByName('user-input')[0];
 const resetbutton = document.getElementById('reset-button');
-//borrar todo
-resetbutton.addEventListener('click', function () { areatexto.value = "" });
-
-areatexto.addEventListener('keyup', function () {
-    const li1 = document.querySelector('.box');
-
-
-
+resetbutton.addEventListener('click', function () { areatexto.value = "" });//boton borrar todo
+areatexto.addEventListener('keyup', function (){
+  const characterCountLi = document.querySelector('.box:nth-child(1)');//obtengo el elemento con el api queryselector, getelements ,etc que es funcion del api, al obtener el elemento le asigno a una constante para poder modificar un atributo de ese elemento
+  const characterCountExcludingSpacesli = document.querySelector('.box:nth-child(2)');
+  const wordCountli = document.querySelector('.box:nth-child(3)');
+  const numberCountli = document.querySelector('.box:nth-child(4)');
+  const numberplusli = document.querySelector('.box:nth-child(5)');
+  const averageWordlengthli = document.querySelector('.box:nth-child(6)');
+  const characterCount = analyzer.getCharacterCount(areatexto.value);//analyzer tiene sus funciones , getcharas
+  const wordCount = analyzer.getWordCount(areatexto.value);
+  characterCountLi.innerHTML = `Caracteres: ${characterCount}`; //literal string ${};template strings es una string que contiene variables en su interior, para hacer el llamado a la variable se utiliza "${}"
+  wordCountli.innerHTML = `Palabras: ${wordCount}`;
+  const numberCount = analyzer.getNumberCount(areatexto.value);
+  numberCountli.innerHTML = `Numeros: ${numberCount}`;
+  const numberplus = analyzer.getNumberSum(areatexto.value);
+  numberplusli.innerHTML = `Suma de números: ${numberplus}`;
+  const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(areatexto.value);
+  characterCountExcludingSpacesli.innerHTML = `Caracteres sin espacios: ${characterCountExcludingSpaces}`;
+  const averageWordlength = analyzer.getAverageWordLength(areatexto.value);
+  averageWordlengthli.innerHTML = `Promedio longitud: ${averageWordlength}`;
 });
-
-
 
 
 
